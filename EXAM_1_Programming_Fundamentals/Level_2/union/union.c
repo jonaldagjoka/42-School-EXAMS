f61dacd26db2331f795a576074a8b0d561597a46
Jonalda <122224333+jl999lj@users.noreply.github.com>
2026-01-19 20:48:01 +0100

    Level_2 finished

--- EXAM_1_Programming_Fundamentals/Level_2/union/union.c
+++ EXAM_1_Programming_Fundamentals/Level_2/union/union.c
@@ -1,7 +1,30 @@
 #include <unistd.h>
 int main(int ac,char *av[]){
-    if(ac==2){
-        
-    }
+    if(ac==3){
+       int lookup[256]={};
+       int i=0;
+       while(av[1][i])
+            lookup[(int)av[1][i++]]=1;
+        i=0;
+       while(av[2][i])
+            lookup[(int)av[2][i++]]=1;
+        i=0;    
+        while(av[1][i]){
+            if(lookup[(int)av[1][i]]){
+                write(1,&av[1][i],1);
+                lookup[(int)av[1][i]]=0;
+            }
+            i++;
+        }
+        i=0;
+        while(av[2][i]){
+           if(lookup[(int)av[2][i]]){
+            write(1,&av[2][i],1);
+            lookup[(int)av[2][i]]=0;
+           }
+           i++;
+        }
+       } 
+    write(1, "\n", 1);
     return 0;
 }
